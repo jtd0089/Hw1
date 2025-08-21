ae7752c42d5a80b0cc0fa35e84a8901d69277643
jtd0089 <jtd0089@auburn.edu>
2025-08-21 15:44:51 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "starter.h"
+
+using namespace std;
+
+int main()
+{
+	int start_num1 = 20;
+	int start_num2 = 15;			//create a few starter objects
+
+    int final_num = start_num1 + start_num2;
+
+    cout << start_num1 << " + " << start_num2 << " = " << final_num << endl; 
+	//print out all the values for each object to validate your code
+}
